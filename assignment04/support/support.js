33bade21a424c72d3ecac152af19446aeb30167a
Heng <hengkov653@gmail.com>
2024-05-30 19:05:51 +0700

    done

--- assignment04/support/support.js
+++ assignment04/support/support.js
@@ -36,3 +36,25 @@ function applyTheme(theme) {
         document.body.style.backgroundColor = 'white';
     }
 }
+
+// ---------------------increase and decrease number
+const plus = document.querySelector(".plus"),
+minus = document.querySelector(".minus"),
+number = document.querySelector(".number");
+let a=1;
+
+plus.addEventListener("click",()=>{
+a++;
+a= (a<10) ? "0" +a : a;
+number.innerText = a ;
+console.log("a");
+});
+
+
+minus.addEventListener("click",()=>{
+if(a>1){
+    a--;
+    a= (a<10) ? "0" +a : a;
+    number.innerText = a ;
+}
+});
